12df56bb4d1a219ee031fad1817d45d21c9dbdea
Hanif Setyananda <hanifsetya@dinustek.com>
2022-06-08 14:14:53 +0700

    push awal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,8 +41,8 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+          <Link to="/" className="navbar-item" title="Logo">
+            <h1><b>HANIF</b></h1>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,26 +63,19 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
+          
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+             
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/hanifsetyananda"
                 target="_blank"
                 rel="noopener noreferrer"
               >
---
+++ src/pages/blog/2022-06-07-first-post.md
@@ -0,0 +1,13 @@
+---
+templateKey: blog-post
+title: First Post
+date: 2022-06-07T18:09:21.566Z
+description: just started make a blog!
+featuredpost: true
+featuredimage: /img/sus.jpg
+tags:
+  - story
+---
+i'm happy setting this up! 
+
+even though it just plug and play, i'll fill this with anything i want!
